6e926f9ccfe5a27787f24cd681e0ee247f6dce2f
COFII <0000chris11@gmail.com>
2020-10-10 22:28:18 -0300

    first time using JavaFX

---
+++ src/JTable_ex/Main.java
@@ -0,0 +1,61 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package JTable_ex;
+
+import javafx.beans.property.SimpleStringProperty;
+import javafx.beans.property.StringProperty;
+import javafx.collections.ObservableList;
+import javax.swing.AbstractAction;
+import javafx.scene.control.TableView;
+
+/**
+ *
+ * @author Christopher
+ */
+public class Main {
+
+      private StringProperty firstName;
+      private String firstN;
+
+      public void setFirstName(String value) {
+            firstNameProperty().set(value);
+      }
+
+      public String getFirstName() {
+            return firstNameProperty().get();
+      }
+
+      public StringProperty firstNameProperty() {
+            if (firstName == null) {
+                  firstName = new SimpleStringProperty(this, "firstName");
+            }
+            return firstName;
+      }
+      //+++++++++++++++++++++++++++++++++++++++++++++
+      private StringProperty lastName;
+      private String lastN;
+
+      public void setLastName(String value) {
+            lastNameProperty().set(value);
+      }
+
+      public String getLastName() {
+            return lastNameProperty().get();
+      }
+
+      public StringProperty lastNameProperty() {
+            if (lastName == null) {
+                  lastName = new SimpleStringProperty(this, "lastName");
+            }
+            return lastName;
+      }
+      //+++++++++++++++++++++++++++++++++++++++++++++
+      public static void main(String[] args) {
+            ObservableList<String> ddd = 
+            TableView table = new TableView();
+            
+      }
+}
